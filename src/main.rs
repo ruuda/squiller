85b7c500c9d84a0c20cbf290dec11743e66e24f1
Ruud van Asseldonk <dev@veniogames.com>
2022-06-19 22:51:50 +0200

    Add basic driver program
    The program reads files passed on the command line, runs the parser on
    them, and prints them back, for debugging purposes for now.

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+use squoiler::lexer::sql::Lexer;
+use squoiler::parser::document::Parser;
+
+fn main() {
+    for arg in std::env::args().skip(1) {
+        let input = std::fs::read(arg).expect("Failed to read input file.");
+        let tokens = Lexer::new(&input).run();
+        let mut parser = Parser::new(&input, &tokens);
+        while let Ok(section) = parser.parse_section() {
+            let section_str = section.resolve(&input);
+            println!("{:#?}", section_str);
+        }
+    }
+}
