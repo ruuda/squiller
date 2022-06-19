1bdda0496e8da3ecd9fb5936b281e815adc92861
Ruud van Asseldonk <dev@veniogames.com>
2022-06-20 00:03:43 +0200

    Add readme, license, and contribution guide

--- src/ast.rs
+++ src/ast.rs
@@ -1,3 +1,10 @@
+// Querybinder -- Generate boilerplate from SQL for statically typed languages
+// Copyright 2022 Ruud van Asseldonk
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// A copy of the License has been included in the root of the repository.
+
 use crate::Span;
 
 /// Types of parameters and results.
