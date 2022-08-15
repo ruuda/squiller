b4226c0d150adef0c3903f9468d07278ab53d781
Ruud van Asseldonk <dev@veniogames.com>
2022-08-15 22:42:57 +0200

    Add fuzzer that hits the typecheck phase
    This is the same fuzzer as the "parse" fuzzer (which was broken after
    my module rename -- fix it), but with the additional typecheck stage
    on top. I could have one fuzzer, but this leaves more room for the
    parser fuzzer to run faster, should I modify the parser.

--- fuzz/fuzz_targets/typecheck.rs
+++ fuzz/fuzz_targets/typecheck.rs
@@ -3,6 +3,7 @@
 use libfuzzer_sys::fuzz_target;
 use querybinder::lexer::document::Lexer;
 use querybinder::parser::document::Parser;
+use querybinder::typecheck;
 
 type Error = Box<dyn querybinder::error::Error>;
 
@@ -10,11 +11,12 @@ fn handle_input(input: &str) -> Result<(), Error> {
     let lexer = Lexer::new(&input);
     let tokens = lexer.run()?;
     let mut parser = Parser::new(&input, &tokens);
-    let _ = parser.parse_document()?;
+    let doc = parser.parse_document()?;
+    let _ = typecheck::check_document(&input, doc)?;
     Ok(())
 }
 
 fuzz_target!(|input: &str| {
-    // Parsing may fail, but it should not hang or panic.
+    // Processing may result in an error, but it should not hang or panic.
     let _ = handle_input(input);
 });
