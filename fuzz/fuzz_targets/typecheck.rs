5394e08aac8d79608dcbb8e79944bb3dccd29db9
Ruud van Asseldonk <dev@veniogames.com>
2022-08-20 22:09:21 +0200

    Simplify the "typecheck" fuzzer
    Now that I have the NamedDocument that handles all steps, let's use it.

--- fuzz/fuzz_targets/typecheck.rs
+++ fuzz/fuzz_targets/typecheck.rs
@@ -1,21 +1,10 @@
 #![no_main]
 
 use libfuzzer_sys::fuzz_target;
-use querybinder::error::Result;
-use querybinder::lexer::document::Lexer;
-use querybinder::parser::document::Parser;
-use querybinder::typecheck;
+use std::path::PathBuf;
 
-fn handle_input(input: &str) -> Result<()> {
-    let lexer = Lexer::new(&input);
-    let tokens = lexer.run()?;
-    let mut parser = Parser::new(&input, &tokens);
-    let doc = parser.parse_document()?;
-    let _ = typecheck::check_document(&input, doc)?;
-    Ok(())
-}
-
-fuzz_target!(|input: &str| {
+fuzz_target!(|input_bytes: &[u8]| {
     // Processing may result in an error, but it should not hang or panic.
-    let _ = handle_input(input);
+    let fname: PathBuf = "fuzz".into();
+    let _ = querybinder::NamedDocument::process_input(&fname, input_bytes);
 });
